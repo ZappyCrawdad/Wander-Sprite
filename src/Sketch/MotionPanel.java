5e0b063aa6157e7c46bded37c67a70fc527087fc
Raynou <rafaelgame2012@gmail.com>
2022-03-02 17:50:37 -0600

    añadir movimiento a la ventana 1

---
+++ src/Sketch/MotionPanel.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Sketch;
+
+import static com.sun.java.accessibility.util.AWTEventMonitor.addMouseListener;
+import static com.sun.java.accessibility.util.AWTEventMonitor.addMouseMotionListener;
+import java.awt.Point;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author rafaelgarciamendoza
+ */
+public class MotionPanel extends JPanel{
+    private Point initialClick;
+    private JFrame parent;
+
+    public MotionPanel(final JFrame parent){
+    this.parent = parent;
+
+    addMouseListener(new MouseAdapter() {
+        public void mousePressed(MouseEvent e) {
+            initialClick = e.getPoint();
+            getComponentAt(initialClick);
+        }
+    });
+
+    addMouseMotionListener(new MouseMotionAdapter() {
+        @Override
+        public void mouseDragged(MouseEvent e) {
+
+            // get location of Window
+            int thisX = parent.getLocation().x;
+            int thisY = parent.getLocation().y;
+
+            // Determine how much the mouse moved since the initial click
+            int xMoved = e.getX() - initialClick.x;
+            int yMoved = e.getY() - initialClick.y;
+
+            // Move window to this position
+            int X = thisX + xMoved;
+            int Y = thisY + yMoved;
+            parent.setLocation(X, Y);
+        }
+    });
+    }
+}
